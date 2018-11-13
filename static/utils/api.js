6ba4cf1306693260e40cf4356bc260fe882941e1
wu.yong@neusoft.com <wu.yong@NEUSOFT.INTERNAL>
2018-11-13 11:20:56 +0800

    init

---
+++ static/utils/api.js
@@ -0,0 +1,18 @@
+export default {
+    getData() {
+        return [
+            {"id": "1", "content": "喜欢什么体育运动", "options": [
+                {"key": "1", "value": "游泳"}, {"key": "2", "value": "健身"}, {"key": "3", "value": "打篮球"}, {"key": "4", "value": "踢足球"}
+            ]},
+            {"id": "2", "content": "喜欢玩什么游戏", "options": [
+                {"key": "1", "value": "王者荣耀"}, {"key": "2", "value": "英雄联盟"}, {"key": "3", "value": "守望先锋"}, {"key": "4", "value": "星际争霸"}
+            ]},
+            {"id": "3", "content": "什么职业", "options": [
+                {"key": "1", "value": "工人"}, {"key": "2", "value": "农民"}, {"key": "3", "value": "人民教师"}, {"key": "4", "value": "个体户"}
+            ]},
+            {"id": "4", "content": "平时喜欢看什么报纸", "options": [
+                {"key": "1", "value": "大连晚报"}, {"key": "2", "value": "半岛晨报"}, {"key": "3", "value": "新商报"}, {"key": "4", "value": "人民日报"}
+            ]}
+        ]
+    }
+}
